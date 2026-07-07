4fa1524a53e53432f008919351fda2730e1e25f8
Lisett <lisettbracamontes@gmail.com>
2026-07-06 20:14:02 -0600

    bug fixing

--- server/src/controllers/documentoController.js
+++ server/src/controllers/documentoController.js
@@ -149,4 +149,44 @@ const previsualizarDocumento = async (req, res) => {
     res.status(500).json({ error: 'Error al previsualizar documento' })
   }
 }
-module.exports = { subirDocumento, listarDocumentos, descargarDocumento, eliminarDocumento, previsualizarDocumento }
+
+// Editar documento
+const editarDocumento = async (req, res) => {
+  try {
+    const { titulo, descripcion, tipo } = req.body
+
+    const actualizar = {}
+    if (titulo) actualizar.titulo = titulo
+    if (descripcion !== undefined) actualizar.descripcion = descripcion
+    if (tipo) actualizar.tipo = tipo
+
+    if (req.file) {
+      actualizar.archivo = req.file.buffer
+      actualizar.archivoNombre = req.file.originalname
+      actualizar.archivoTipo = req.file.mimetype
+    }
+
+    const documento = await Documento.findOneAndUpdate(
+      { _id: req.params.docId, equipoId: req.params.teamId },
+      actualizar,
+      { new: true }
+    ).select('-archivo')
+
+    if (!documento) return res.status(404).json({ error: 'Documento no encontrado' })
+
+    await registrarLog({
+      empleadoId: req.user.id,
+      numeroEmpleado: req.user.numeroEmpleado,
+      accion: 'EDITAR_DOCUMENTO',
+      detalle: `Editó el documento "${documento.titulo}"`,
+      ip: req.ip,
+      exitoso: true
+    })
+
+    res.json(documento)
+  } catch (err) {
+    res.status(500).json({ error: 'Error al editar documento' })
+  }
+}
+
+module.exports = { subirDocumento, listarDocumentos, descargarDocumento, eliminarDocumento, previsualizarDocumento, editarDocumento }
